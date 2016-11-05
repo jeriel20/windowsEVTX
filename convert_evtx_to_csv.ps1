f37672fdd43e18af26e035f46dcfa49019cd4d1b
Jeriel Juarbe <jeriel.juarbe@gmail.com>
2016-11-05 15:39:18 -0400

    Add files via upload

---
+++ convert_evtx_to_csv.ps1
@@ -0,0 +1,65 @@
+#########################################
+####	Jeriel Juarbe Script		#####
+####	20161101					#####
+####	EVTX Conversions to CSV		#####
+####	USING LOGPARSER.EXE			#####
+#########################################
+#-=--=-=--=-=--=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
+#																	#
+#	This script can be ran from anywhere on the Filesystem			#
+#	You must have LogParser2.2 installed							#
+#	You may need to run the following command to allow PS1 to run	#
+#																	#
+#			C:\>	Set-Execution-Policy Unrestricted				#
+#																	#
+#-=--=-=--=-=--=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
+
+#Input from User#
+$folder_location = read-host "Please enter the full path of the file locations - IE: C:\Users\"
+$dest_location = read-host "Please enter the full path of destination directiory to dump the files - IE: C:\Users\Admin\Desktop\"
+$logparser_location = read-host "Please enter the full path of LogParser.exe - IE: C:\Users\Admin\Desktop\LogParser\LogParser.exe"
+
+#Definining Variables for the loops below
+$ext = "*.evtx"
+$src_location = $folder_location + $ext
+$CMD = $logparser_location
+$arg1 = 'SELECT * INTO'
+$arg3 = '-i:EVT'
+$arg4 = '-o:CSV'
+$arg5 = 'FROM'
+$quoteDouble = '"'
+$quoteSingle = "'"
+
+#Array is required to store the name of all the files that will be converted
+$files_array = @()
+$out_files_array = @()
+
+write-host "STANDBY while the conversion is completed........OUTPUT to STDOUT to follow...."
+
+#This date command below is to keep track of how long it took to convert the EVTX files to CSV
+date
+
+#This Foreach loop gets all the file names in the directiory specified and stores their names in an ARRAY for future use	#
+$a = Get-Item $src_location
+foreach ($file in $a) {
+	$files_array += $file.Fullname
+	$out_files_array += $dest_location + $file.BaseName + ".csv"
+}
+
+
+# This While loop - while $i  is less that the length of the Array (from above) 
+# creates 2 new arrays ($in_log) and ($out_log) that contains the String values for output destination of CSV and Input location of EVTX
+# Then runs the logparser.exe with switches 
+# 
+$i = 0
+while ($i -lt $files_array.length) {
+
+	$in_log = $files_array[$i]
+	$out_log = $out_files_array[$i]
+
+	& $CMD $quoteDouble$arg1 $quoteSingle$out_log$quoteSingle $arg5 $quoteSingle$in_log$quoteSingle$quoteDouble $arg3 $arg4
+	$i += 1
+}
+
+#This date commande below is to keep track of how long it took to convert the EVTX files to CSV
+date	
---
+++ untar.bat
@@ -0,0 +1,32 @@
+REM #########################################
+REM ####	LT Milbank Script			#####
+REM ####	20161101					#####
+REM ####	Jeriel Juarbe{				#####
+REM ####	Unpack TAR.GZ &				#####
+REM ####	Unzip all ZIP's				#####
+REM ####	USING 7z} 					#####
+REM #########################################
+REM #-=--=-=--=-=--=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=--=--=--=-=--=-=-==-#
+REM #																									#
+REM #	This script is designed to be ran from the directory where your (filename.tar.gz) files reside	#
+REM #	You must have 7z installed on your system														#
+REM #	You must add the binary path for 7z to the windows Enviromental variable Path					#
+REM #	**IF you only have ZIP'd Files then comment out the first FOR loop.								#
+REM #-=--=-=--=-=--=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=--=-=-=-=-=-=-=-=--=#
+@Echo Off
+mkdir tmp
+
+REM this for loop takes all .GZ files in the current directory and uncompresses them
+REM Then the TAR Files are PIPED to another 7z commnand that uncompresses the TARs into the TMP Directory you created
+FOR /F "usebackq" %%a in (`DIR /s /b *.gz`) do 7z x %%a -r -so | 7z x -si -ttar -o"TMP"
+
+REM This FOR loop unzips all the files in the TMP directory you created 
+FOR /F "usebackq" %%a in (`DIR /s /b *.zip`) do 7z x %%a -o"TMP"
+
+mkdir EVTX
+
+REM This FOR loop movesa all the uncompresses .evtx files to the EVTX you created
+FOR /F "usebackq" %%a in (`DIR /s /b *.evtx`) do move %%a EVTX
+
+REM this below Removes the TMP folder to clean up the mess
+rmdir /S /Q tmp
